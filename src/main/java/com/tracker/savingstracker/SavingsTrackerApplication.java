e4deba78fab5572e3e04c1f17218d9c1b8b620f6
James <clubjec@gmail.com>
2026-04-26 16:32:12 +0100

    initial commit

---
+++ src/main/java/com/tracker/savingstracker/SavingsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.savingstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavingsTrackerApplication.class, args);
+    }
+
+}
