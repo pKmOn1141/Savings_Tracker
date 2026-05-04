83326b603a61f437f62ae11219c5e0db98585724
James <clubjec@gmail.com>
2026-05-04 01:13:20 +0100

    accounts to csv

--- src/main/java/com/tracker/savingstracker/service/CsvWriter.java
+++ src/main/java/com/tracker/savingstracker/service/CsvWriter.java
@@ -5,18 +5,26 @@ import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.tracker.savingstracker.model.Account;
 import com.tracker.savingstracker.model.BalanceEntry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
+@Service
 public class CsvReader {
+
+    @Value("${app.csv.directory}")
+    private String path;
+
     public CsvReader() {
     }
 
     // Write account info to csv
     private void writeAccountInfo(Account account) throws Exception {
-        try (Writer writer = new FileWriter("accountInfo.csv")) {
+        try (Writer writer = new FileWriter(pathGen(account.getName(), "accountInfo.csv"))) {
             StatefulBeanToCsv<Account> beanToCsv = new StatefulBeanToCsvBuilder<Account>(writer).build();
 
             beanToCsv.write(account);
@@ -24,8 +32,10 @@ public class CsvReader {
     }
 
     // Write account entries to csv
-    private void writeAccountEntries(BalanceEntry[] entries) throws IOException {
-        try (CSVWriter writer = new CSVWriter(new FileWriter("entries.csv"))) {
+    private void writeAccountEntries(Account account) throws IOException {
+        BalanceEntry[] entries = account.getEntries();
+
+        try (CSVWriter writer = new CSVWriter(new FileWriter(pathGen(account.getName(), "entries.csv")))) {
             writer.writeNext(new String[]{
                     "date", "depositAmount", "accountBalance", "poundChange", "percentChange"
             });
@@ -44,11 +54,18 @@ public class CsvReader {
     }
 
     // Generates the path string for the csv file
-    private String pathGen(String name) {
-
+    private String pathGen(String account, String type) throws IOException {
+        return new StringBuilder()
+                .append(path)
+                .append(File.separator)
+                .append(account)
+                .append(File.separator)
+                .append(type)
+                .toString();
     }
 
-    public void writeEntireAcc(Account account) throws IOException {
-        private String
+    public void writeEntireAcc(Account account) throws Exception {
+        writeAccountInfo(account);
+        writeAccountEntries(account);
     }
 }
